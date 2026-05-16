94b74f21498e0317758cb617f9e5181bc9d25b6a
Cebastion <99135718+Cebastion@users.noreply.github.com>
2026-05-16 16:03:16 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/lab_13/", // Обов'язково з косими рисками з обох боків!
+});
